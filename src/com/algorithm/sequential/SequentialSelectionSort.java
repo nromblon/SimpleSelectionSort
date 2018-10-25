4637b788c92127748bbefc4aab354df3f014a3dc
Neil Romblon <neilromblon@live.com>
2018-10-25 11:55:51 +0800

    converted int[] to List<Integer>. setup also done

--- src/com/algorithm/sequential/SequentialSelectionSort.java
+++ src/com/algorithm/sequential/SequentialSelectionSort.java
@@ -1,30 +1,36 @@
 package com.algorithm.sequential;
 
+import java.util.List;
+
+/* This code is adapted from Rajat Mishra's*/
+
 public class SequentialSelectionSort {
-    public void sort(int arr[]) { 
-        int n = arr.length; 
-  
-        // One by one move boundary of unsorted subarray 
-        for (int i = 0; i < n-1; i++) { 
-            // Find the minimum element in unsorted array 
-            int min_idx = i; 
-            for (int j = i+1; j < n; j++) 
-                if (arr[j] < arr[min_idx]) 
-                    min_idx = j; 
-  
-            // Swap the found minimum element with the first 
-            // element 
-            int temp = arr[min_idx]; 
-            arr[min_idx] = arr[i]; 
-            arr[i] = temp; 
-        } 
-    } 
-  
-    // Prints the array 
-    public void printArray(int arr[]) { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
+    public void sort(List<Integer> list) {
+        int n = list.size();
+
+        // One by one move boundary of unsorted sublistay
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted listay
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (list.get(j) < list.get(min_idx))
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = list.get(min_idx);
+            list.set(min_idx,list.get(i));
+            list.set(i,temp);
+        }
+    }
+
+    // Prints the array
+    public void printArray(List<Integer> arr)
+    {
+        int n = arr.size();
+        for (int i=0; i<n; ++i)
+            System.out.print(arr.get(i)+" ");
+        System.out.println();
+    }
 }
--- src/com/main/Driver.java
+++ src/com/main/Driver.java
@@ -1,18 +1,41 @@
 package com.main;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.algorithm.parallel.ParallelSelectionSort;
+import com.algorithm.sequential.SequentialSelectionSort;
+import com.reusables.CsvParser;
 
 public class Driver {
 	public static void main(String[] args) {
-		ParallelSelectionSort parallelSelectionSort = new ParallelSelectionSort();
-		ArrayList<Integer> itemList = new ArrayList<Integer>();
-		itemList.add(5);
-		itemList.add(3);
-		itemList.add(1);
-		itemList.add(2);
-		itemList.add(4);
-		
-		parallelSelectionSort.sort(itemList);
+	    runSequential();
 	}
+
+	public static void runParallel(){
+
+        ParallelSelectionSort parallelSelectionSort = new ParallelSelectionSort();
+        ArrayList<Integer> itemList = new ArrayList<Integer>();
+        itemList.add(5);
+        itemList.add(3);
+        itemList.add(1);
+        itemList.add(2);
+        itemList.add(4);
+
+        parallelSelectionSort.sort(itemList);
+    }
+
+	public static void runSequential(){
+        String filename = "10000random.csv";
+        SequentialSelectionSort ob = new SequentialSelectionSort();
+        List<Integer> input = CsvParser.read(filename);
+        Instant start = Instant.now();
+        ob.sort(input);
+        Instant end = Instant.now();
+        System.out.println("Sorted array");
+        ob.printArray(input);
+        long duration = Duration.between(start,end).toMillis();
+        System.out.println("List of size "+input.size()+" took "+duration+"ms to be sorted.");
+    }
 }
