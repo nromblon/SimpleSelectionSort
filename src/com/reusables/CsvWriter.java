cce38c881d5387a1d027d0caa6b9e0c25eafafd4
Neil Romblon <neilromblon@live.com>
2018-10-31 23:24:52 +0800

    writer

---
+++ src/com/reusables/CsvWriter.java
@@ -0,0 +1,14 @@
+package com.reusables;
+
+import java.io.BufferedWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+
+public class CsvWriter {
+
+    public static void write(ArrayList<Integer> list){
+        String filename = "asd";
+        BufferedWriter writer = new BufferedWriter(new Writer() {
+        });
+    }
+}
