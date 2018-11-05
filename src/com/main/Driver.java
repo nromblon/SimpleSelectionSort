40fa5ddacd0ddf558149789bdc6b5286d8ffcaea
Neil Romblon <neilromblon@live.com>
2018-11-05 18:34:48 +0800

    gen number + example

--- src/com/main/Driver.java
+++ src/com/main/Driver.java
@@ -17,65 +17,61 @@ public class Driver {
 	private static final String generated_name_prefix = "unsorted_generated_";
 
 	public static void main(String[] args) {
-
+        ArrayList<Integer> input = generateRandom(10000);
 //		runParallel_forkjoin();
 //        runParallel_executor();
 //        runParallel_lambda();
 //        System.out.println("\n\nStarting sequential...");
-        runSequential();
+        runSequential(input);
 //        System.out.println("\n\nStarting parallel standard...");
 //        runParallel();
 	}
-	public static void runParallel_forkjoin(){
+	public static void runParallel_forkjoin(ArrayList<Integer> list){
 		ParallelSelectionForkJoin parallelSelectionForkJoin = new ParallelSelectionForkJoin();
-        ArrayList<Integer> itemList = new ArrayList<Integer>();
-
-        itemList = CsvParser.read(filename);
+        ArrayList<Integer> itemList = list;
         parallelSelectionForkJoin.start(itemList, 2);
 	}
-	public static void runParallel_lambda(){
+	public static void runParallel_lambda(ArrayList<Integer> list){
         ParallelSelectionLambda pLambda = new ParallelSelectionLambda();
-        ArrayList<Integer> itemList = new ArrayList<Integer>();
-
-        itemList = CsvParser.read(filename);
+        ArrayList<Integer> itemList = list;
 
 		System.out.println();
         pLambda.parallelSelectionSort(itemList,2);
     }
 	
-	public static void runParallel_executor(){
+	public static void runParallel_executor(ArrayList<Integer> list){
 
         ParallelSelectionExecutor parallelSelectionExecutor = new ParallelSelectionExecutor();
-        ArrayList<Integer> itemList = new ArrayList<Integer>();
-
-        itemList = CsvParser.read(filename);
+        ArrayList<Integer> itemList = list;
         parallelSelectionExecutor.start(itemList, 2);
     }
-	public static void runParallel(){
+	public static void runParallel(ArrayList<Integer> list){
 
         ParallelSelectionSort parallelSelectionSort = new ParallelSelectionSort();
-        ArrayList<Integer> itemList = new ArrayList<Integer>();
-
-        itemList = CsvParser.read(filename);
+        ArrayList<Integer> itemList = list;
         parallelSelectionSort.start(itemList, 2);
     }
 
-	public static void runSequential(){
+	public static void runSequential(ArrayList<Integer> list){
 //        SequentialSelectionSort seq = new SequentialSelectionSort();
         RunnableSequentialSelectionSort runSeq = new RunnableSequentialSelectionSort();
-        ArrayList<Integer> input = CsvParser.read(filename);
+        ArrayList<Integer> input = list;
         runSeq.start(input);
 
     }
 
     public static ArrayList<Integer> generateRandom(int amount){
+        System.out.println("Generating input...");
         Random rand = new Random();
         rand.setSeed(Instant.now().getEpochSecond());
         ArrayList<Integer> numbers = new ArrayList<>();
         for(int i = 0; i<amount; i++){
-            numbers.add(rand.nextInt());
+            numbers.add(Math.abs(rand.nextInt()));
         }
+        System.out.println("Input generation complete.");
+        System.out.println("Writing into a file...");
         CsvWriter.write(numbers, generated_name_prefix+amount+".csv");
+        System.out.println("Write complete.\n==========================");
         return numbers;
     }
 }
